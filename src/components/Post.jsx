c224d6270df09efac43a4a22d450fbac0ffb815b
namartiinss <nathaliamartins0708@gmail.com>
2025-03-30 01:33:57 -0300

    realizando likes nos comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,7 +10,7 @@ import styles from "./Post.module.css";
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
 
-  const [newCommentText, setNewCommentText] = useState("");
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(
     publishedAt,
@@ -33,16 +33,23 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório!");
+  }
+
   function onDeleteComment(commentToDelete) {
-    const commentsWithoutDeletedOne = comments.filter(comment => {
-        return comment ==! commentToDelete;
-    })    
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
 
     setComments(commentsWithoutDeletedOne);
-}
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -85,10 +92,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit"  disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
